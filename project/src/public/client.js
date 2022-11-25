e360975e0606053d006edb0bd719b59f6c98f618
Ali Alnasser <ali.row2@hotmail.com>
2022-11-25 17:55:24 +0300

    final version

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -6,8 +6,6 @@ let store = {
 // add our markup to the page
 const root = document.getElementById('root')
 
-let immutable = Immutable.Map({test:"test"})
-
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
     render(root, store)
@@ -18,14 +16,14 @@ const updateDate = (newDate) =>{
 	store = Object.assign(store,{date:newDate})
 }
 
+
 const storePhotos=(store,images)=>{
-	store= Object.assign(store,images)
+	store = Object.assign(store,images)
 }
 
 const render = async(root, state) => {
     root.innerHTML = App(state)
 }
-
 
 // create content
 const App = (state) => {
@@ -38,9 +36,9 @@ const App = (state) => {
 			<p>Welcome to the dashboard of mars rovers, please feel free to view below rovers<br>
 			<section>
 				<div class="buttonContainer">
-					<div class="roverButton" onClick='getNewestImages(0)'>Curiosity</div>
-					<div class="roverButton" onClick='getNewestImages(1)'>Spirit</div>
-					<div class="roverButton" onClick='getNewestImages(2)'>Opportunity</div>
+					<div class="roverButton" id="Curiosity" onClick='getNewestImages(0,Curiosity)'>Curiosity</div>
+					<div class="roverButton" id="Spirit" onClick='getNewestImages(1,Spirit)'>Spirit</div>
+					<div class="roverButton" id="Opportunity" onClick='getNewestImages(2,Opportunity)'>Opportunity</div>
 				</div>
                 <div id="roverInfo" class="roverInfo" >
                 
@@ -59,20 +57,16 @@ window.addEventListener('load', () => {
 })
 
 // ------------------------------------------------------  COMPONENTS
-
-
-
 // This part used to print information related to the rover
 
-
 const createDataOfRoverImages = (index) =>{	
-		const createImageGallery =(index)=>{
-			return store.rovers[index].images.toJS().map(
+	const createImageGallery=()=>{
+		return store.rovers[index].images.toJS().map(
 			(image) =>{return `<img src="${image}" class="roverImage" />`}
 			).join('')
-		}
+	}
 		rover = store.rovers[index]
-    return `rover "${rover.name}" has been launched in ${rover.launch_date}<br>
+    const roverData = `rover "${rover.name}" has been launched in ${rover.launch_date}<br>
 Rover's maximum solar days are ${rover.max_sol} and it has captured ${rover.total_photos} since landing date in ${rover.landing_date}<br>
 rover's last images were in date ${rover.max_date} and it's current status is <b>${rover.status}</b><br>
 rover "${rover.name}" has ${rover.cameras.length} cameras<br>
@@ -81,13 +75,10 @@ some of rover's images:<br><br><br>
 ${createImageGallery(index)}
 </div>
 `
+		return ()=>{ // saving data specified to specific rover
+			return roverData
+		}
 }
-
-
-const roverInfoPrint = (index) => {
-		document.getElementById("roverInfo").innerHTML = createDataOfRoverImages(index)
-}
-
 
 // ------------------------------------------------------  Controlling style
 
@@ -97,9 +88,18 @@ const resetClasses=()=>{
 	selectedButtons[0].classList.replace("roverButtonSelected","roverButton")
 }
 
-const setSelectedClass=(index)=>{
+
+const setSelectedClass=(index,rover)=>{
+	console.log(rover)
 	const btn = document.getElementsByClassName("roverButton")[index].classList;
-	btn.replace("roverButton","roverButtonSelected")
+	const selectedBtn = document.getElementById(rover)
+	console.log(selectedBtn)
+	return ()=>{
+		const selectedButtons = document.getElementsByClassName("roverButtonSelected")
+		if(!selectedButtons.length==0){selectedButtons[0].classList.replace("roverButtonSelected","roverButton")}
+		 selectedBtn.className="roverButtonSelected"
+	}
+	
 }
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
@@ -117,6 +117,7 @@ const Greeting = (name) => {
 
 // ------------------------------------------------------  API CALLS
 // Responsible to update current rovers into fully detailed rovers data
+
 const getRoversData = () => {
     fetch(`http://localhost:3000/rovers`)
         .then(res => res.json())
@@ -124,8 +125,6 @@ const getRoversData = () => {
 }
 
 const getNewestImages = async(currentIndex) => {
-	resetClasses()
-	setSelectedClass(currentIndex)
     const rovers = store.rovers
 	const date = new Date();
 	//in case images is created for first time it will call the API and save it in immutable in case  
@@ -136,10 +135,12 @@ const getNewestImages = async(currentIndex) => {
 		const promise = await fetch(`http://localhost:3000/rovers/${rovers[currentIndex].name}`)
 			.then(res => res.json())
 		storePhotos(rovers[currentIndex],{images:Immutable.List(promise.pics.latest_photos.map(photo=>{return photo.img_src}))})
-roverInfoPrint(currentIndex)
+		storePhotos(rovers[currentIndex], {printImages:createDataOfRoverImages(currentIndex)})
+		storePhotos(rovers[currentIndex], {setButton:setSelectedClass(currentIndex,rovers[currentIndex].name)})
+		
 updateDate(date)
 }
-else{
-document.getElementById("roverInfo").innerHTML = createDataOfRoverImages(currentIndex)
-}
+document.getElementById("roverInfo").innerHTML = store.rovers[currentIndex].printImages()
+store.rovers[currentIndex].setButton()
+
 }
